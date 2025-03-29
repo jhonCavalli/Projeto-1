f42e88c5d75f013c2be9613a8092e77e7222e920
jhon Cavalli <jhoncavalli@hotmail.com>
2025-03-29 14:08:52 -0300

    adiciona configuração inicial para o gráfico de desempenho no script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const ctx = document.getElementById('performanceChart').getContext('2d');
+const performanceChart = new Chart(ctx, {
+    type: 'line',
+    data:{
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [{
